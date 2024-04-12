c274f25d4e16fbbefe429117a52b171b94bddad4
Root <Pughy_123@outlook.com>
2024-04-12 16:30:18 +0100

    Created callback timer

---
+++ Runtime/CallbackTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Packages.callback_timer
+{
+    public class CallbackTimer
+    {
+        private readonly CallbackTimerBehaviour behaviour;
+
+        public CallbackTimer(Action callback, float duration)
+        {
+            behaviour = new GameObject("CallbackTimer").AddComponent<CallbackTimerBehaviour>();
+            behaviour.Configure(callback, duration);
+        }
+
+        public CallbackTimer Start()
+        {
+            behaviour.Begin();
+            return this;
+        }
+
+        public CallbackTimer Stop()
+        {
+            behaviour.Stop();
+            return this;
+        }
+
+        public CallbackTimer Pause()
+        {
+            behaviour.Pause();
+            return this;
+        }
+    }
+}
---
+++ Runtime/CallbackTimerBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Packages.callback_timer
+{
+    public class CallbackTimerBehaviour : MonoBehaviour
+    {
+        private float duration;
+        private Action callback;
+        private float currentTime;
+        
+        private bool paused = true;
+
+        public void Configure(Action callback, float duration)
+        {
+            this.callback = callback;
+            this.duration = duration;
+            
+            currentTime = duration;
+        }
+
+        private void Update()
+        {
+            if (paused) return;
+            
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                callback?.Invoke();
+                Stop();
+            }
+        }
+
+        public void Begin()
+        {
+            paused = false;
+        }
+
+        public void Stop()
+        {
+            paused = true;
+            Destroy(this.gameObject);
+        }
+        
+        public void Pause()
+        {
+            paused = true;
+        }
+    }
+}
